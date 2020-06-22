282db4c78d045b6c12dd33284265f0a23160190b
Marcin <marcuck21@gmail.com>
2020-06-22 19:03:53 +0200

    ocr test

--- run.py
+++ run.py
@@ -1,2 +1,7 @@
-print("Hello world!")
-print("test")
+from PIL import Image
+import pytesseract
+
+im = Image.open("test_img/test1.jpg")
+text = pytesseract.image_to_string(im, lang='eng')
+print(text)
+
