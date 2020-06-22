a944d464da86fce35a09bf51877fbc080c4df096
Marcin <marcuck21@gmail.com>
2020-06-22 19:59:27 +0200

    ocr test

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import pytesseract
 
-im = Image.open("test_img/test1.jpg")
-text = pytesseract.image_to_string(im, lang='eng')
+im = Image.open("test_img/parag_01.jpg")
+text = pytesseract.image_to_string(im, lang='pol')
 print(text)
 
